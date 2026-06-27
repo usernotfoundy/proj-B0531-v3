14a36207efd2bce247f035f988ee7d40158cf8ad
Emman Adonay <adonayemmanuelii@gmail.com>
2026-06-28 02:02:02 +0800

    added gdrive links in archive

--- src/config/babyPhotos.config.ts
+++ src/config/babyPhotos.config.ts
@@ -22,6 +22,54 @@ export const BABY_PHOTO_GALLERY: PhotoGalleryData = {
     subtitle: 'Captured moments from Xyz\'s first trip around the sun.',
     tabs: [
         {
+            id: 'solo',
+            label: 'Xyz',
+            photos: [
+                { 
+                    src: driveImage('https://drive.google.com/file/d/1_Q6f-Y0sz4WmF97rSPP6VbDytwu70kGv/view?usp=sharing'), 
+                    alt: 'Sweet smile', 
+                    colSpan: 4, 
+                    rowSpan: 3 
+                },
+                { 
+                    src: driveImage('https://drive.google.com/file/d/11OEVEBQKa_3rVq9bXrFxgOm-4LPcnENU/view?usp=sharing'), 
+                    alt: 'Little hands', 
+                    colSpan: 2, 
+                    rowSpan: 4 
+                },
+                { 
+                    src: driveImage('https://drive.google.com/file/d/16w-SLBv1ptzLEUrey6fIvNc1ap-LpMVp/view?usp=sharing'), 
+                    alt: 'Peek-a-boo', 
+                    colSpan: 2, 
+                    rowSpan: 2 
+                },
+                { 
+                    src: driveImage('https://drive.google.com/file/d/1-guOTu95ikE_x3NgGxyBDntTLvY4oYV3/view?usp=sharing'), 
+                    alt: 'Giggles', 
+                    colSpan: 2, 
+                    rowSpan: 2 
+                },
+                { 
+                    src: driveImage('https://drive.google.com/file/d/1XEDb9FSTpXQguqCsgD79y-RgKNCoqf8w/view?usp=sharing'), 
+                    alt: 'Cuteness Overload', 
+                    colSpan: 2, 
+                    rowSpan: 4
+                },
+                { 
+                    src: driveImage('https://drive.google.com/file/d/1eDLBJb9wuNBeQHt6m7vdNKC2R3nGN4ZI/view?usp=sharing'), 
+                    alt: 'Day 1', 
+                    colSpan: 2, 
+                    rowSpan: 3 
+                },
+                { 
+                    src: driveImage('https://drive.google.com/file/d/1DG9p_zaPZMCacE_d8UU5PrnGUcxy-CR3/view?usp=sharing'), 
+                    alt: 'Hep hep! Hooray!!', 
+                    colSpan: 2, 
+                    rowSpan: 3 
+                },
+            ],
+        },
+        {
             id: 'family',
             label: 'Family',
             photos: [
@@ -31,28 +79,32 @@ export const BABY_PHOTO_GALLERY: PhotoGalleryData = {
                     colSpan: 4,
                     rowSpan: 3,
                 },
-                {   src: driveImage('https://drive.google.com/file/d/1l9UGkwiEk9B4Uub2uSLr-5iOqDQXjnb1/view?usp=sharing'), 
+                {   
+                    src: driveImage('https://drive.google.com/file/d/1l9UGkwiEk9B4Uub2uSLr-5iOqDQXjnb1/view?usp=sharing'), 
                     alt: 'Together', 
                     colSpan: 2, 
                     rowSpan: 4 
                 },
-                { src: 'https://i.pinimg.com/736x/ad/b4/ca/adb4ca5f4e4b74bfa095660f6fba9cf5.jpg', alt: 'Growing together', colSpan: 2, rowSpan: 2 },
-                { src: 'https://i.pinimg.com/1200x/b3/16/f0/b316f069f7b214e923f0960af5ea5db6.jpg', alt: 'Cozy day', colSpan: 2, rowSpan: 2 },
-                { src: 'https://i.pinimg.com/736x/c9/e6/64/c9e664d0f811cf2995465e76ec594263.jpg', alt: 'Play time', colSpan: 2, rowSpan: 3 },
-                { src: 'https://i.pinimg.com/736x/fd/3e/d3/fd3ed3a334508d1e9db7cdf36490ac8e.jpg', alt: 'First steps', colSpan: 3, rowSpan: 2 },
+                {   
+                    src: driveImage('https://drive.google.com/file/d/1gEZ2vTOW9yHiLOSUb2T8TMprPz94_1HU/view?usp=sharing'), 
+                    alt: 'Growing together', 
+                    colSpan: 2, 
+                    rowSpan: 6
+                },
+                {   
+                    src: driveImage('https://drive.google.com/file/d/1mgcfoYZRJ80bHnappEMt0i0GIEJCTLaK/view?usp=sharing'), 
+                    alt: 'Cozy day', 
+                    colSpan: 2, 
+                    rowSpan: 6 
+                },
+                { 
+                    src: driveImage('https://drive.google.com/file/d/18s31tMAdZSe2GsJNqH5xPekNAaDUgdWw/view?usp=sharing'), 
+                    alt: 'Play time', 
+                    colSpan: 2, 
+                    rowSpan: 5 
+                },
             ],
         },
-        {
-            id: 'solo',
-            label: 'Solo',
-            photos: [
-                { src: 'https://i.pinimg.com/736x/c2/d4/69/c2d469f496e1e01579101c14159fc190.jpg', alt: 'Sweet smile', colSpan: 4, rowSpan: 3 },
-                { src: 'https://i.pinimg.com/736x/69/36/fb/6936fb46d238367c39c6ff57c6b69e8a.jpg', alt: 'Little hands', colSpan: 2, rowSpan: 4 },
-                { src: 'https://i.pinimg.com/1200x/c0/1d/51/c01d5194b8d2a0e987e6d38937a2e64b.jpg', alt: 'Peek-a-boo', colSpan: 2, rowSpan: 2 },
-                { src: 'https://i.pinimg.com/736x/39/f6/7a/39f67a2ccf9d8191207825286e7e4f30.jpg', alt: 'Giggles', colSpan: 2, rowSpan: 2 },
-                { src: 'https://i.pinimg.com/736x/b5/72/a0/b572a0cec03948e0896bc584b2ab7f22.jpg', alt: 'Bath day', colSpan: 2, rowSpan: 3 },
-                { src: '/babies/baby.png', alt: 'Xyz', colSpan: 2, rowSpan: 2 },
-            ],
-        },
+        
     ],
 }
